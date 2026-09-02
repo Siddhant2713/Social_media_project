7c0639965ea91a0f83ca18617558cada140a7609
Siddhant2713 <siddhantramakant@gmail.com>
2026-09-02 11:59:38 +0530

    User model

---
+++ server/models/user.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+
+    profileImage:{
+        type: String 
+    },
+
+    followers: [],
+    
+    followings:[],
+
+    posts: [],
+
+    stories: [],
+
+
+}, {timestamps: true})
+
+const userModel = mongoose.model("User", userSchema);
+
+export default User 
+
